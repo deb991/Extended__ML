d01a55c5347f35615e58b2ebeafe3b3fdf13ae78
Ozzius <deb991@users.noreply.github.com>
2019-03-22 21:53:31 +0530

    Pandas & MatplotLib is included for future purpose
    Pandas & MatplotLib is included for future purpose

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python -i
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ds = pd.read_csv("C:\\Users\\DE635273\\PycharmProjects\\TG_proj\\datasets\\data.csv")
+print('\nData Set loaded')  
+
+print(ds.shape)
+
+for cols in ds.columns:
+    print(cols)
+    print(ds.iloc[0,:])
+    print(ds.loc[0:5,:])
+    break
+        
+print((ds.loc[:5,['Age', 'Acceleration']]))
+print('\nEOP')
+
+print(ds['Aggression'])
+print(ds[['Age', 'Aggression', 'Aggression']])
+
+
+print('\nCreating/modifying new Pandas Series')
+s1 = pd.Series([1,2])
+s2 = pd.Series(['Bob','Marley'])
+print(s2)
+
+print('\n Now creating A DataFrame\n')
+df = pd.DataFrame([s1, s2])
+print(df)
+
+df1 = pd.DataFrame([[1,2], ['Bob','Marley']], index=['Row1', 'Row2'] , columns=['First__Name', 'Last__Name'])
+print(df1)
+
+df2 = pd.DataFrame(
+        {'column1' : [577, "Dad's home"],
+         'column2' : [3699, "Jey's home"]
+         }
+        )
+
+print(df2)
+
+
+print(ds.corr())
+
+ds[ds["Age"] == "Acceleration"]["Value"].plot(kind="")
